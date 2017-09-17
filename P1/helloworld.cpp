33c9bd1739172e1c3136ff439ee084f66b34ff8d
Sushant <sushant.s.raikar@gmail.com>
2017-09-17 17:24:13 -0400

    server ready

--- P1/helloworld.cpp
+++ P1/helloworld.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
-
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdexcept>
+#include <unistd.h>
 using namespace std;
+
+void error(string);
 
 int main()
 {
-        cout << "Hello World!" << endl;
-            return 0;
+    int portNum = 7777;
+ 
+    int client = socket(AF_INET, SOCK_STREAM, 0);
+    if( client < 0)
+        error("couldnot open socket");
+    
+    struct sockaddr_in server_address, client_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htons(INADDR_ANY);
+    server_address.sin_port = htons(portNum);
+
+    if ((bind(client, (struct sockaddr*)&server_address, sizeof(server_address))) < 0) 
+        error("couldnot bind socket");    
+    socklen_t size = sizeof(server_address);               
+    listen(client, 1);
+    int server = accept(client, (struct sockaddr *)&server_address, &size);
+
+    int bufsize = 1024;
+    char buffer[bufsize];
+    while (server < 0 )
+    {
+        recv(server, buffer, bufsize, 0);
+        cout << buffer << endl;
+        if(*buffer == '*')
+            break;
+    }
+    close(server);
+}
+
+void error(string error)
+{
+    throw runtime_error(error);
 }
