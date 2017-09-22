9eee807d3e968054388e9ebe74e83195c164301b
Sushant <sushant.s.raikar@gmail.com>
2017-09-22 11:04:40 -0400

    Q3 solution

---
+++ P1/q3client.cpp
@@ -0,0 +1,100 @@
+// Client side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <iostream>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdexcept>
+#include <unistd.h>
+#define PORT 8080
+#define REPEAT 10
+  
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    cout << "Q3 Socket Creation Overhead" << endl << "PartA" << endl;
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "X";
+    char buffer[1024] = {0};
+    clock_t beginn, endd;
+    for(int i = 0; i< REPEAT; i++){
+        // Experiment 1
+        beginn = clock();
+        for(int i =0; i<1000; i++){
+            if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+            {
+                printf("\n Socket creation error \n");
+                return -1;
+            }
+          
+            memset(&serv_addr, '0', sizeof(serv_addr));
+          
+            serv_addr.sin_family = AF_INET;
+            serv_addr.sin_port = htons(PORT);
+              
+            // Convert IPv4 and IPv6 addresses from text to binary form
+            if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+            {
+                printf("\nInvalid address/ Address not supported \n");
+                return -1;
+            }
+            if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+            {
+                printf("\nConnection Failed \n");
+                return -1;
+            }
+            send(sock , hello , 1 , 0 );
+            valread = read( sock , buffer, 1024);
+            close(sock);
+        }
+        endd = clock();
+        double partA = double(endd - beginn)/ CLOCKS_PER_SEC;
+        cout << partA << endl;
+        sleep(1);
+    }
+    cout << "PartB" << endl;
+    // Experiment 2
+    hello = "Z";
+    for(int i=0; i<REPEAT; i++){
+        beginn = clock();
+        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        {
+            printf("\n Socket creation error \n");
+            return -1;
+        }
+      
+        memset(&serv_addr, '0', sizeof(serv_addr));
+      
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(PORT);
+          
+        // Convert IPv4 and IPv6 addresses from text to binary form
+        if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+        {
+            printf("\nInvalid address/ Address not supported \n");
+            return -1;
+        }
+        if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        {
+            printf("\nConnection Failed \n");
+            return -1;
+        }
+        for(int i =0; i<1000; i++){ 
+            send(sock , hello , 1 , 0 );
+            valread = read( sock , buffer, 1024);
+        }
+        close(sock);
+        endd = clock();
+        double partB = double(endd - beginn)/ CLOCKS_PER_SEC;
+        cout << partB << endl;
+    }
+    return 0;
+}
