1ac3410ebec5934d9a2aa338b959a4c4863a076d
Sushant <sushant.s.raikar@gmail.com>
2017-10-03 20:06:35 -0400

    q4 server.cpp

---
+++ P1/q4server.cpp
@@ -0,0 +1,77 @@
+/*
+    Simple udp server
+*/
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <iostream>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdexcept>
+#include <unistd.h>
+ 
+#define BUFLEN 512  //Max length of buffer
+#define PORT 8888   //The port on which to listen for incoming data
+using namespace std;
+void die(char *s)
+{
+    perror(s);
+    exit(1);
+}
+ 
+int main(void)
+{
+    struct sockaddr_in si_me, si_other;
+     
+    int s, i, recv_len;
+    socklen_t slen = sizeof(si_other);
+    char buf[BUFLEN];
+     
+    //create a UDP socket
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+        die("socket");
+    }
+     
+    // zero out the structure
+    memset((char *) &si_me, 0, sizeof(si_me));
+     
+    si_me.sin_family = AF_INET;
+    si_me.sin_port = htons(PORT);
+    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+     
+    //bind socket to port
+    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
+    {
+        die("bind");
+    }
+     
+    //keep listening for data
+    long counter = 0;
+    printf("Waiting for data...");
+    while(1)
+    {
+        fflush(stdout);
+        usleep(100);
+        //try to receive some data, this is a blocking call
+        if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
+        {
+            die("recvfrom()");
+        }
+        if(buf[0] == 'X'){
+            cout << counter << endl << "Waiting for data..." << endl;
+            counter = 0;
+        }
+        else {
+            counter ++;
+        }    
+    }
+ 
+    close(s);
+    return 0;
+}
